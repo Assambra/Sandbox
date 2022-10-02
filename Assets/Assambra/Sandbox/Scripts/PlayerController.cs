d12bae096022ca903a4abda2f8c498c878b535b0
Philipp Leutner <6276777+Assambra@users.noreply.github.com>
2022-10-02 18:34:16 +0200

    Code cleanup PlayerController

--- Assets/Assambra/Sandbox/Scripts/PlayerController.cs
+++ Assets/Assambra/Sandbox/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,47 +6,69 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 2.0f;
-    public float runSpeedFactor = 0f;
-    public float rotationSpeed = 150f;
-    public float mouseRotateSpeedFactor = 2f;
-    public float mouseX;
-    public float mouseY;
+    [Header("Serialize fields")]
+    [SerializeField] private CameraController cameraController = null;
 
-    private void Start()
+    [Header("Movement speed")]
+    [SerializeField] private float movementSpeed = 2f;
+    
+
+    [Header("Rotation speed")]
+    [SerializeField] private float keyRotationSpeed = 150f;
+    [SerializeField] private float mouseRotationSpeed = 300f;
+
+    // Private variables
+    private float runSpeedFactor = 0f;
+
+    private Vector3 movement = Vector3.zero;
+    private Vector3 mouseRotation = Vector3.zero;
+
+
+    private void Awake()
     {
+        cameraController = GameObject.FindObjectOfType<CameraController>();
+
+        if (cameraController == null)
+            Debug.LogError("No CameraController found!");
     }
 
     void Update()
     {
-        // Get Player Input to move forwards and backwards
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        
-        // while shift pressed increase the runspeed by this factor
-        if (Input.GetKey(KeyCode.LeftShift))
-            runSpeedFactor = 2.0f;
-        else
-            runSpeedFactor = 1f;
+        GetAxisInput();
+        HandleMoveSpeed();
 
         //We want to rotated the character with mouse input if right mouse button pressed
         if (Input.GetMouseButton(1))
         {
-            mouseX = Input.GetAxis("Mouse X") * rotationSpeed * mouseRotateSpeedFactor * Time.deltaTime;
-            
+            mouseRotation.y = cameraController.MouseX * mouseRotationSpeed * Time.deltaTime;
             // Rotate the Character with Mouse
-            transform.Rotate(new Vector3(0, mouseX, 0));
+            transform.Rotate(new Vector3(0, mouseRotation.y, 0));
 
             // Move the character
-            transform.Translate(move * moveSpeed * runSpeedFactor * Time.deltaTime);
+            transform.Translate(movement * movementSpeed * runSpeedFactor * Time.deltaTime);
         }
         // rotate with Keys and block moving x
         else
         {
             // Rotate the Character with keys
-            transform.Rotate(0, move.x * rotationSpeed * Time.deltaTime, 0);
+            transform.Rotate(0, movement.x * keyRotationSpeed * Time.deltaTime, 0);
             
             // Move the character but dont move in x
-            transform.Translate(new Vector3(0, 0, move.z) * moveSpeed * runSpeedFactor * Time.deltaTime);
+            transform.Translate(new Vector3(0, 0, movement.z) * movementSpeed * runSpeedFactor * Time.deltaTime);
         }
+    }
+
+    private void GetAxisInput()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+
+    private void HandleMoveSpeed()
+    {
+        // while shift pressed increase the runspeed by this factor
+        if (Input.GetKey(KeyCode.LeftShift))
+            runSpeedFactor = 2.0f;
+        else
+            runSpeedFactor = 1f;
     }
 }
